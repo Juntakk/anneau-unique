bb1e5b8103112440e2b58f91762d528196bdbaa3
Nicolas Gauthier <nick.habashi@gmail.com>
2025-06-15 23:39:16 -0400

    Refactor code structure for improved readability and maintainability

---
+++ components/shared/user-avatar.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+
+const UserAvatar = ({ name, image }: { name: string; image: string }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  return (
+    <>
+      <div className="flex flex-col items-center gap-2">
+        <div
+          className={cn(
+            "text-black font-semibold mb-8 text-4xl font-[MedievalSharp] tracking-widest",
+            isHovered ? "text-black" : "text-background"
+          )}
+        >
+          {name}
+        </div>
+        <Avatar
+          className="w-60 h-60 hover:transform hover:scale-110 transition-transform duration-300 ease-in-out hover:cursor-pointer"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          onClick={() => {
+            alert(name);
+          }}
+        >
+          <AvatarImage src={image} alt={name} />
+          <AvatarFallback>{name.charAt(0)}</AvatarFallback>
+        </Avatar>
+      </div>
+    </>
+  );
+};
+
+export default UserAvatar;
