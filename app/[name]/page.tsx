87e669da7d79c72db0a1bd515d3c895c72a9722c
Nicolas Gauthier <nick.habashi@gmail.com>
2025-06-16 23:07:24 -0400

    Fix parameter type definition in Home component for correct async handling

--- app/[name]/page.tsx
+++ app/[name]/page.tsx
@@ -1,13 +1,11 @@
 import MainPage from "./components/main-page";
 
 type Props = {
-  params: {
-    name: string;
-  };
+  params: Promise<{ name: string }>;
 };
 
 const Home = async ({ params }: Props) => {
-  const { name } = params;
+  const { name } = await params;
 
   return <MainPage name={name} />;
 };
