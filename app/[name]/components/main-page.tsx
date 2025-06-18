9031323e69a60bd8fe93c7b1e55a77d7e20dd152
Nicolas Gauthier <nick.habashi@gmail.com>
2025-06-17 21:48:38 -0400

    Remove background color from MainPage component for consistent styling

--- app/[name]/components/main-page.tsx
+++ app/[name]/components/main-page.tsx
@@ -4,7 +4,7 @@ import MainInput from "./main-input";
 
 const MainPage = ({ name }: { name: string }) => {
   return (
-    <div className="bg-green-900 min-h-screen text-foreground flex-col p-6">
+    <div className=" min-h-screen text-foreground flex-col p-6">
       <MainInput name={name} label="Nom" />
       <div className="flex">
         <MainInput label="Culture" />
