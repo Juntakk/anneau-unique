4e6b0a2ed600f5b7921f64f88a1a01576cef6129
Nicolas Gauthier <nicolas.gauthier@leadsonline.com>
2025-06-28 15:00:07 -0400

    feat: add value and onChange props to FormField component for better state management

--- app/[name]/components/character-sheet/community-phase-section.tsx
+++ app/[name]/components/character-sheet/community-phase-section.tsx
@@ -1,10 +1,10 @@
-import FormField from "../ui/form-field";
+import FormField from '../ui/form-field';
 
 export default function CommunityPhaseSection() {
   return (
     <div className="flex flex-col text-left w-full p-12 gap-4">
-      <FormField label="Sanctuaires" />
-      <FormField label="Garant" />
+      <FormField label="Sanctuaires" value="" />
+      <FormField label="Garant" value="" />
     </div>
   );
 }
