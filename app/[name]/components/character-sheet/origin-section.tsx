fd153e175de9d92031b8b782319a07ce5b6639cd
Juntakk <nick.habashi@gmail.com>
2025-07-04 16:42:32 -0400

    feat: integrate user context into OriginSection; pass user data to LinedTextarea for 'origine' field

--- app/[name]/components/character-sheet/origin-section.tsx
+++ app/[name]/components/character-sheet/origin-section.tsx
@@ -1,10 +1,12 @@
+import { useUser } from "@/providers/UserContext";
 import LinedTextarea from "../ui/lined-textarea";
 
 export default function OriginSection() {
+  const user = useUser();
   return (
-    <div className="flex flex-col text-center w-full p-12">
+    <div className='flex flex-col text-center w-full p-12'>
       <h2>- ORIGINE -</h2>
-      <LinedTextarea rows={5} />
+      <LinedTextarea rows={5} user={user} field='origine' />
     </div>
   );
 }
