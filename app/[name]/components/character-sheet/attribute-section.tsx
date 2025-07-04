9348b3654f31c16fb6f4923bf26b4d1fb71060f4
Juntakk <nick.habashi@gmail.com>
2025-07-04 19:49:01 -0400

    refactor: standardize string quotes in AttributesSection component

--- app/[name]/components/character-sheet/attribute-section.tsx
+++ app/[name]/components/character-sheet/attribute-section.tsx
@@ -5,24 +5,24 @@ export default function AttributesSection() {
   return (
     <div>
       <SectionHeader>– ATTRIBUTS –</SectionHeader>
-      <div className="flex w-full justify-evenly items-center mb-12">
+      <div className='flex w-full justify-evenly items-center mb-12'>
         <AttributeCircle
-          outerLabel="Corps"
-          innerLabel="Amélioré"
-          field="corps"
-          secondaryField="corps_ameliore"
+          outerLabel='Corps'
+          innerLabel='Amélioré'
+          field='corps'
+          secondaryField='corps_ameliore'
         />
         <AttributeCircle
-          outerLabel="Coeur"
-          innerLabel="Amélioré"
-          field="coeur"
-          secondaryField="coeur_ameliore"
+          outerLabel='Coeur'
+          innerLabel='Amélioré'
+          field='coeur'
+          secondaryField='coeur_ameliore'
         />
         <AttributeCircle
-          outerLabel="Esprit"
-          innerLabel="Amélioré"
-          field="esprit"
-          secondaryField="esprit_ameliore"
+          outerLabel='Esprit'
+          innerLabel='Amélioré'
+          field='esprit'
+          secondaryField='esprit_ameliore'
         />
       </div>
     </div>
