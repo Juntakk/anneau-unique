6834621cbe7cae39f4ff6be6a1a45ffa8d3d246e
Juntakk <nick.habashi@gmail.com>
2025-07-05 01:22:16 -0400

    feat: enhance CommunauteSection to include user data handling and input updates; add origine field to User model

--- app/[name]/components/ui/communaute-circle.tsx
+++ app/[name]/components/ui/communaute-circle.tsx
@@ -1,22 +1,65 @@
-const stats = ['Communauté', 'Points de Progression', 'Trésors', 'Prestige'];
+import { updateUserField } from "@/lib/actions/user.actions";
+import { handleEnterBlur, shouldSkipBlur } from "@/lib/utils";
+import { useUser } from "@/providers/UserContext";
+import { User } from "@/types/user";
+import { useState } from "react";
+import { toast } from "sonner";
 
-const CommunauteCircle = ({ label }: { label: string }) => (
-  <div className="flex flex-col items-center">
-    <h2 className="text-2xl text-center mb-2">{label}</h2>
-    <input
-      type="text"
-      maxLength={1}
-      className="w-40 h-40 rounded-full border-2 border-foreground text-center text-4xl font-bold bg-transparent outline-none"
-    />
-  </div>
-);
+const stats = [
+  { label: "Communauté", field: "communaute" },
+  { label: "Points de Progression", field: "points_progression" },
+  { label: "Trésors", field: "tresors" },
+  { label: "Prestige", field: "prestige" },
+];
 
 export default function CommunauteSection() {
+  const user = useUser();
   return (
-    <div className="flex w-full justify-around items-center mb-12">
-      {stats.map((label) => (
-        <CommunauteCircle key={label} label={label} />
+    <div className='flex w-full justify-around items-center mb-12'>
+      {stats.map((stat, i) => (
+        <CommunauteCircle
+          key={i}
+          field={stat.field as keyof User}
+          label={stat.label}
+          user={user}
+        />
       ))}
     </div>
   );
 }
+
+const CommunauteCircle = ({
+  label,
+  field,
+  user,
+}: {
+  label: string;
+  field: keyof User;
+  user: User;
+}) => {
+  const [value, setValue] = useState((user[field] as string) || "0");
+
+  const handleBlur = async () => {
+    if (shouldSkipBlur()) return;
+    if (value === user[field]) return;
+    const res = await updateUserField(user.id, field, value);
+    toast(res.message);
+  };
+
+  return (
+    <div className='flex flex-col items-center'>
+      <h2 className='text-2xl text-center mb-2'>{label}</h2>
+      <input
+        type='text'
+        maxLength={1}
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+        onBlur={handleBlur}
+        onKeyDown={handleEnterBlur(handleBlur)}
+        className='w-40 h-40 rounded-full border-2 border-foreground text-center text-4xl font-bold bg-transparent outline-none'
+      />
+    </div>
+  );
+};
