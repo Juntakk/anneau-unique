82c5665b5ea6fa589bd99ba9cc1488d39f39e606
Juntakk <nick.habashi@gmail.com>
2025-07-08 23:57:30 -0400

    refactor: simplify PrismaClient initialization by removing global variable handling

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,3 @@
 import { PrismaClient } from "@/generated/prisma";
-
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
-const prisma = global.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
-
+const prisma = new PrismaClient();
 export default prisma;
